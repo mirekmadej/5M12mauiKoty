871c24c38a1d08986be492fc57fe720caa7d764e
Mirosław Madej <M.Madej@zsp3zamosc.pl>
2024-11-15 11:52:38 +0100

    kompletne

--- 5M12mauiKoty/App.xaml.cs
+++ 5M12mauiKoty/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿namespace _5M12mauiKoty
+﻿
+namespace _5M12mauiKoty
 {
     public partial class App : Application
     {
@@ -6,7 +7,18 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            //MainPage = new AppShell();
+        }
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            Window window = new Window();
+            window.MinimumHeight = 800;
+            window.MaximumHeight = 800;
+            window.MinimumWidth = 600;
+            window.MaximumWidth = 600;
+            window.Title = "wykonał: rs";
+            window.Page = new AppShell();
+            return window;
         }
     }
 }
